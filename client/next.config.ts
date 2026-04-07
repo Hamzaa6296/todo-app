963725b055cf992ea679a2c205e8688af75b95ed
Ali Hamza <ali.hamzaaa6296@gmail.com>
2026-04-07 20:27:58 +0500

    updated

--- client/next.config.ts
+++ client/next.config.ts
@@ -17,13 +17,13 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  // ADD THIS SECTION FOR THE DEFAULT SIGNUP PAGE
+  
   async redirects() {
     return [
       {
-        source: '/',           // When the user hits the main URL
-        destination: '/signup', // Send them here
-        permanent: true,       // Tells search engines this is the intended path
+        source: '/',         
+        destination: '/signup', 
+        permanent: true,      
       },
     ];
   },
